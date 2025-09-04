da0e40b1360303a54c861f111088c5d039e8d198
Jagga0177 <aslonovjasurbek@gmail.com>
2025-09-04 09:58:36 +0300

    Create hw1.py

---
+++ Lesson-1/Homework/hw1.py
@@ -0,0 +1,23 @@
+Question 1: 
+a = 4
+P = 4*a
+S = pow(a,2)
+print(P)
+print(S)
+Question 2: 
+import math
+print (math.pi)
+d = 5
+L = math.pi*d
+print(L)
+Question 3: 
+a = 2
+b = 3
+print((a+b)/2)
+Question 4: 
+a = 4
+b = 5
+print(a+b)
+print(a*b)
+print(a**2)
+print(b**2)
